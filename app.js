f7c6de92da4a9aef40649d593e82cfd904401a28
mjhmjh1104 <mjhmjh1104@naver.com>
2017-08-05 08:02:18 +0900

    updated

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get("/", function(req, res) {
+  console.log("Someone got wrong direction!");
+});
+
+app.get("/gp", function(req, res) {
+  console.log("Someone got to grandpa zone!");
+  res.sendfile("gp.html");
+});
+
+io.on('connection', function(socket) {
+  console.log("Someone connected! : " + socket.id);
+  socket.on('disconnect', function() {
+    console.log("Someone disconnected! : " + socket.id);
+  });
+});
+
+
+http.listen('3000', function() {
+  console.log("Server on!");
+});
